ccaadd49baa4c681367326108e79d2addc9575e5
GuilemTheGolem <113972554+GuilemTheGolem@users.noreply.github.com>
2022-10-13 10:05:18 +0200

    Add files via upload

---
+++ juego.py
@@ -0,0 +1,16 @@
+import random
+numaleatorio = random.randint(0,100)
+
+num1(int(input("Dime un número:"))
+if(num1 == numaleatorio):
+     print("Has ganado")
+     else:
+     num2(int(input("Dime un número:"))
+          if(num2 == numaleatorio):
+          print("Has ganado")
+          else:
+          num3(int(input("Dime un número:"))
+               if(num3 == numaleatorio):
+               print("Has ganado")
+               
+print("Has perdido")
